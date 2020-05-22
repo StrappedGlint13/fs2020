4607a9e97a2df8157abc91dc05a0f928d9958963
StrappedGlint13 <matiasbrax@gmail.com>
2020-05-22 18:51:36 +0300

    2.9 done

--- osa2/puhelinluettelo/src/index.js
+++ osa2/puhelinluettelo/src/index.js
@@ -4,10 +4,15 @@ import Person from './components/Person'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244'}
+    { name: 'Arto Hellas', number: '040-1231244'},
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
+  const [ filter, setFilter] = useState('')
+  
 
   const addNew = (event) => {
     event.preventDefault()
@@ -36,11 +41,19 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const filteredNames = persons.filter(person => person.name.toUpperCase().includes(filter.toUpperCase()))
+
   return (
-    
     <div>
-      
-      <h2>Phonebook</h2>
+      <h2> Phonebook </h2>
+      <div>
+        filter shown with <input value={filter} onChange={handleFilterChange} />
+    </div>
+      <h2>add a new</h2>
       <form onSubmit={addNew}>
         <div>
           name: <input value={newName}
@@ -56,7 +69,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <p>
-        {persons.map((person, i) => <Person key={i} person={person} /> 
+        {filteredNames.map((person, i) => <Person key={i} person={person} /> 
         )}
       </p>
     </div>
