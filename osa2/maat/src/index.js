c37e361c7bfb38bd1a783ae0be283c8874722f62
StrappedGlint13 <matiasbrax@gmail.com>
2020-05-25 19:19:06 +0300

    2.12 done

---
+++ osa2/maat/src/index.js
@@ -0,0 +1,83 @@
+import ReactDOM from 'react-dom'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+const Country = props => {
+  return(
+  <div>
+    <h1>{props.country.name}</h1>
+    <div>capital {props.country.capital}</div>
+    <div>population {props.country.population}</div>
+    <h2>languages</h2>
+    <ul>
+    {props.country.languages.map(language => <li key={language.name}>{language.name}</li>)}
+    </ul>
+    <img src={props.country.flag} alt="flag" width="250" height="200" />
+  </div>
+  )
+}
+
+const Countries = props => {
+  const filteredNames = props.countries.filter(country => country.name.toUpperCase().includes(props.filter.toUpperCase()))
+  if(filteredNames.length === 1) {
+    return(
+    <div>
+        <Country country={filteredNames[0]} />
+    </div>
+    )
+  }
+  else if (filteredNames.length > 10) {
+    return (
+      <div>
+        <p>Too many matches, specify another filter</p>
+      </div>
+    )
+  } else if(filteredNames.length < 11) {
+    return(
+    <div>
+      <p>
+        {filteredNames.map(country => <p key={country.name}> {country.name} </p> 
+        )}
+      </p>
+      </div>
+       )
+  } else if(filteredNames.length === 0 || filteredNames.isEmpty()) {
+    return(
+      <div>
+        <p>No matches</p>
+      </div>
+    )
+  }
+}
+
+const Filter = (props) =>
+    <div>
+      find countries <input value={props.filter} onChange={props.handle} />
+    </div>
+
+const App = () => {
+  const [ countries, setCountries ] = useState([])
+  const [ filter, setFilter ] = useState('')
+
+  useEffect(() => {      
+    axios      
+      .get('https://restcountries.eu/rest/v2/all')      
+      .then(response => {             
+        setCountries(response.data)      })  }, [])  
+
+const handleFilterChange = (event) => {
+  setFilter(event.target.value)
+}
+
+
+  return (
+    <div>
+      <Filter filter={filter} handle={handleFilterChange} />
+      <Countries countries={countries} filter={filter} />
+    </div>
+  )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
