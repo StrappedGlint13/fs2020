df6d59c45e045c20a629ca6cfd512684b8c73f9e
StrappedGlint13 <matiasbrax@gmail.com>
2020-08-19 09:40:45 +0300

    7.5-7.6 done

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -93,13 +93,21 @@ const CreateNew = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content: content.value,
-      author: author.value,
-      info: info.value,
+      content: content.input.value,
+      author: author.input.value,
+      info: info.input.value,
       votes: 0
     })
     history.push('/')
   }
+
+  const reset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
+
 
   return (
     <div>
@@ -107,18 +115,19 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input {...content} />
+          <input {...content.input} />
         </div>
         <div>
           author
-          <input {...author} />
+          <input {...author.input} />
         </div>
         <div>
           url for more info
-          <input {...info} />
+          <input {...info.input} />
         </div>
         <button>create</button>
       </form>
+      <button onClick={() => reset()}>reset</button>
     </div>
   )
 
