a11b8eb34a48a09bae47855bfe12aa8957634889
StrappedGlint13 <matiasbrax@gmail.com>
2020-10-01 11:45:44 +0300

    9.10 done

--- osa9/backend_patientor/index.ts
+++ osa9/backend_patientor/index.ts
@@ -1,13 +1,20 @@
 import express from 'express';
+import diagnoseRouter from './src/routes/diagnoses';
 const app = express();
 app.use(express.json());
+const cors = require('cors')
 
 const PORT = 3001;
+
+app.use(cors())
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnoseRouter)
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
