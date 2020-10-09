a331b0e0e7bddfb2a13d1071111f72e8e3054d43
StrappedGlint13 <matiasbrax@gmail.com>
2020-10-09 17:32:37 +0300

    9.15 done

--- osa9/courses/my-app/src/App.tsx
+++ osa9/courses/my-app/src/App.tsx
@@ -1,24 +1,16 @@
 import React, {  } from "react";
 
 
-interface PartProps {
-  name: string;
-  exerciseCount: number;
-}
-
-
 const Header: React.FC<{ courseName: string}> = ({courseName}) => {
   return <h1> {courseName} </h1>;
 }
 
-const Content: React.FC<{ courseParts: PartProps[]}> = ({courseParts}) => {
+const Content: React.FC<{ courseParts: CoursePart[]}> = ({courseParts}) => {
   return (
     <div>
-    {courseParts.map((course: PartProps) => (
-        <p>
-          {course.name} {course.exerciseCount}
-      </p>
-    ))}
+    {courseParts.map((course: CoursePart) =>
+          <Part key={course.name} part={course} />
+    )}
   </div>
     )
 }
@@ -31,20 +23,118 @@ const Total: React.FC<{ total: number}> = ({total}) => {
     )
 }
 
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
+const Part: React.FC<{ part: CoursePart}> = ({part}) => {
+    switch (part.name) {
+      case "Fundamentals":
+        return(
+          <p>
+          <b>{part.name}</b>
+          <br></br>
+          exercises: {part.exerciseCount}
+          <br></br>
+          description: {part.description}
+        </p>
+        )   
+      case "Using props to pass data":
+        return(
+          <p>
+          <b>{part.name}</b>
+          <br></br>
+          exercises: {part.exerciseCount}
+          <br></br>
+          groupProjectCount: {part.groupProjectCount}
+        </p>
+        )
+      case "Deeper type usage":
+        return (
+          <p>
+          <b>{part.name}</b>
+          <br></br>
+          exercises: {part.exerciseCount}
+          <br></br>
+          exerciseSubmissionLink: {part.exerciseSubmissionLink}
+          <br></br>
+          description: {part.description}
+        </p>
+        )
+      case "Calculation models":
+        return (
+          <p>
+          <b>{part.name}</b>
+          <br></br>
+          exercises: {part.exerciseCount}
+          <br></br>
+          description: {part.description}
+          <br></br>
+          frustration level: {part.frustrationLevel}
+        </p>
+        )
+      default:
+      return assertNever(part);
+    }
+}
+
+interface CoursePartWithDescription {
+  description: string;
+}
+
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+interface CoursePartOne extends CoursePartBase, CoursePartWithDescription {
+  name: "Fundamentals";
+}
+
+interface CoursePartTwo extends CoursePartBase {
+  name: "Using props to pass data";
+  groupProjectCount: number;
+}
+
+interface CoursePartThree extends CoursePartBase, CoursePartWithDescription {
+  name: "Deeper type usage";
+  exerciseSubmissionLink: string;
+}
+
+interface CoursePartFour extends CoursePartBase, CoursePartWithDescription {
+  name: "Calculation models"
+  frustrationLevel: number;
+}
+
+type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree | CoursePartFour;
+
+
 const App: React.FC = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev"
+    },
+    {
+      name: "Calculation models",
+      exerciseCount: 33,
+      description: "Modelling some calculations",
+      frustrationLevel: 0
     }
   ];
 
